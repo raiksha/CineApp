31dd12d0441eba80d88f28b062ca6d779843d3a0
María C. Riquelme <44560427+raiksha@users.noreply.github.com>
2026-04-14 18:24:08 -0400

    feat(CineService): add class structure (#1)
    - added constructor and getters/setters
    - added structure of other functions

--- src/service/CineService.java
+++ src/service/CineService.java
@@ -1,4 +1,61 @@
 package service;
 
+import model.Cliente;
+import model.Funcion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CineService {
+
+    private List<Cliente> listaClientes;
+    private List<Funcion> listaFunciones;
+
+    // Constructor
+    public CineService() {
+        this.listaClientes = new ArrayList<>();
+        this.listaFunciones = new ArrayList<>();
+    }
+
+    // Getters y setters
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public List<Funcion> getListaFunciones() {
+        return listaFunciones;
+    }
+
+    public void setListaFunciones(List<Funcion> listaFunciones) {
+        this.listaFunciones = listaFunciones;
+    }
+
+    // Métodos del servicio
+    public void mostrarMenuService() {
+        // Diego 
+    }
+
+    public void crearCliente() {
+        // Angela 
+    }
+
+    public void verCartelera() {
+        // Julio
+    }
+
+    public void comprarEntrada() {
+        // Constanza
+    }
+
+    public void verEntradasCliente() {
+        // Alexander
+    }
+
+    public void salir() {
+        // Sary
+    }
 }
